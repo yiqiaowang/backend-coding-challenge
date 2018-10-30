fa35907c15e082c3d3786cacfb45071aad95ed76
Yiqiao <yiqiao.wang96@gmail.com>
2018-10-29 21:13:48 -0400

    deploy to heroku

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,10 @@ use Mix.Config
 # which you typically run after static files are built.
 config :suggestions, SuggestionsWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "limitless-shore-64325.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   data: "cities_canada-usa.tsv"
 
 # Do not print debug messages in production
@@ -59,7 +61,3 @@ config :logger, level: :info
 #
 #     config :suggestions, SuggestionsWeb.Endpoint, server: true
 #
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
